86f59bf49d5422f32f9d3c0a8bde1a6f9e95fa3d
Anton Prokhorov <vziks@live.ru>
2017-06-20 20:08:13 +0300

    Fix

--- Repository/SliderRepository.php
+++ Repository/SliderRepository.php
@@ -69,12 +69,15 @@ class SliderRepository extends EntityRepository
 
         $userLocation = $this->container->get('adw.geoip.handler')->getLocation($userIp);
 
+
+
+
         if ($userLocation && $userLocation->getCode() == 'RU') {
 
-            $userCity = $this->container->get('adw.geoip.handler')->getCity($userIp);
+            $userCity = $this->container->get('adw.geoip.handler')->getCity('185.22.181.170');
 
             $relatedCity = $em->getRepository('ADWSliderBundle:RelatedCity')->findOneBy(['prefix' => 'is_' . strtolower($userLocation->getCode()) . '_' . strtolower($this->translit($userCity))]);
-            if (!$relatedCity && $cityRelated->getCity()->getPrefix() && $userCity) {
+            if (!$relatedCity && $userCity) {
                 $relatedCity = new RelatedCity();
                 $relatedCity->setName($userCity);
                 $relatedCity->setPrefix('is_' . strtolower($userLocation->getCode()) . '_' . strtolower($this->translit($userCity)));
@@ -248,7 +251,7 @@ class SliderRepository extends EntityRepository
         $s = str_replace(array("\n", "\r"), " ", $s); // убираем перевод каретки
         $s = preg_replace("/\s+/", ' ', $s); // удаляем повторяющие пробелы
         $s = trim($s); // убираем пробелы в начале и конце строки
-        $s = function_exists('mb_strtolower') ? mb_strtolower($s) : strtolower($s); // переводим строку в нижний регистр (иногда надо задать локаль)
+        $s = function_exists('mb_strtolower') ? mb_strtolower($s, 'UTF-8') : strtolower($s); // переводим строку в нижний регистр (иногда надо задать локаль)
         $s = strtr($s, array('а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch', 'ы' => 'y', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'ъ' => '', 'ь' => ''));
         $s = preg_replace("/[^0-9a-z-_ ]/i", "", $s); // очищаем строку от недопустимых символов
         $s = str_replace(" ", "-", $s); // заменяем пробелы знаком минус
